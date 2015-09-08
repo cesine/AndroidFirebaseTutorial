ad17656fefbcb2569d2a59e722cfbc932335757d
cesine <cesine@yahoo.com>
2015-09-08 09:24:54 -0400

    start firebase

--- Application/src/main/java/com/example/android/basicsyncadapter/EntryListActivity.java
+++ Application/src/main/java/com/example/android/basicsyncadapter/EntryListActivity.java
@@ -3,6 +3,8 @@ package com.example.android.basicsyncadapter;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import com.firebase.client.Firebase;
+
 /**
  * Activity for holding EntryListFragment.
  */
@@ -11,6 +13,8 @@ public class EntryListActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Firebase.setAndroidContext(this);
+
         setContentView(R.layout.activity_entry_list);
     }
 }
