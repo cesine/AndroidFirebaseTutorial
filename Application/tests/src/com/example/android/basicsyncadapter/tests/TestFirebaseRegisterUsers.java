354ae594756ff37941b140fbd219631bb6da9346
cesine <cesine@yahoo.com>
2015-09-08 09:34:44 -0400

    added test for registering users on a firebase

---
+++ Application/tests/src/com/example/android/basicsyncadapter/tests/TestFirebaseRegisterUsers.java
@@ -0,0 +1,43 @@
+package com.example.android.basicsyncadapter.tests;
+
+import android.annotation.TargetApi;
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.example.android.basicsyncadapter.BuildConfig;
+import com.example.android.basicsyncadapter.EntryListActivity;
+import com.firebase.client.Firebase;
+
+public class TestFirebaseRegisterUsers extends ActivityInstrumentationTestCase2<EntryListActivity> {
+
+    private EntryListActivity mTestActivity;
+
+    @TargetApi(8)
+    public TestFirebaseRegisterUsers() {
+        super(EntryListActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        mTestActivity = getActivity();
+        mTestActivity.getSupportFragmentManager().getFragments().get(0);
+    }
+
+    /**
+     * Test if the test fixture has been set up correctly.
+     */
+    public void testPreconditions() {
+        //Try to add a message to add context to your assertions. These messages will be shown if
+        //a tests fails and make it easy to understand why a test failed
+        assertNotNull("mTestActivity is null", mTestActivity);
+    }
+
+    public void testFirebaseRegisterUserWithEmail() {
+        Firebase myFirebaseRef = new Firebase(BuildConfig.FIREBASE_URL);
+        assertNotNull(myFirebaseRef);
+
+        assertEquals(false, true);
+    }
+
+}
