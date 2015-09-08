0ad010b6e8cfc80c8d573e61106508088bb835d1
cesine <cesine@yahoo.com>
2015-09-08 09:53:13 -0400

    registering users test passes

--- Application/tests/src/com/example/android/basicsyncadapter/tests/TestFirebaseRegisterUsers.java
+++ Application/tests/src/com/example/android/basicsyncadapter/tests/TestFirebaseRegisterUsers.java
@@ -2,10 +2,14 @@ package com.example.android.basicsyncadapter.tests;
 
 import android.annotation.TargetApi;
 import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
 
 import com.example.android.basicsyncadapter.BuildConfig;
 import com.example.android.basicsyncadapter.EntryListActivity;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+import java.util.Map;
 
 public class TestFirebaseRegisterUsers extends ActivityInstrumentationTestCase2<EntryListActivity> {
 
@@ -37,7 +41,23 @@ public class TestFirebaseRegisterUsers extends ActivityInstrumentationTestCase2<
         Firebase myFirebaseRef = new Firebase(BuildConfig.FIREBASE_URL);
         assertNotNull(myFirebaseRef);
 
-        assertEquals(false, true);
+        myFirebaseRef.createUser("bobtony@firebase.com", "correcthorsebatterystaple", new Firebase.ValueResultHandler<Map<String, Object>>() {
+            @Override
+            public void onSuccess(Map<String, Object> result) {
+                Log.d(TestFirebaseRegisterUsers.class.getSimpleName(), "Successfully created user account with uid: " + result.get("uid"));
+                assertNotNull(result);
+                assertNotNull(result.get("uid"));
+            }
+
+            @Override
+            public void onError(FirebaseError firebaseError) {
+                // there was an error
+                Log.e(TestFirebaseRegisterUsers.class.getSimpleName(), firebaseError.getDetails());
+                Log.e(TestFirebaseRegisterUsers.class.getSimpleName(), firebaseError.getMessage());
+                assertTrue(firebaseError.getMessage().contains("The specified email address is already in use."));
+            }
+        });
+
     }
 
 }
