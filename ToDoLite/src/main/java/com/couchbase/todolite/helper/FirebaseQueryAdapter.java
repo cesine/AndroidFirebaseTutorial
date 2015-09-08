b07546367bdf4aa9bf53c53675c60e0bbef5c088
cesine <cesine@yahoo.com>
2015-09-08 17:45:17 -0400

    firebase adapter
    based on https://github.com/firebase/AndroidChat/blob/master/app/src/main/java/com/firebase/androidchat/FirebaseListAdapter.java

---
+++ ToDoLite/src/main/java/com/couchbase/todolite/helper/FirebaseQueryAdapter.java
@@ -0,0 +1,189 @@
+package com.couchbase.todolite.helper;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.couchbase.lite.Database;
+import com.couchbase.todolite.BuildConfig;
+import com.couchbase.todolite.document.TaskModel;
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.Query;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @param <T> The class type to use as a model for the data contained in the children of the given Firebase location
+ * @author greg
+ * @since 6/21/13
+ * <p/>
+ * https://github.com/firebase/AndroidChat/blob/master/app/src/main/java/com/firebase/androidchat/FirebaseListAdapter.java
+ * <p/>
+ * This class is a generic way of backing an Android ListView with a Firebase location.
+ * It handles all of the child events at the given Firebase location. It marshals received data into the given
+ * class type. Extend this class and provide an implementation of <code>populateView</code>, which will be given an
+ * instance of your list item mLayout and an instance your class that holds your data. Simply populate the view however
+ * you like and this class will handle updating the list as the data changes.
+ */
+public abstract class FirebaseQueryAdapter<T> extends BaseAdapter {
+
+    private Query mRef;
+    private List<TaskModel> mModels;
+    private List<String> mKeys;
+    private ChildEventListener mListener;
+    private Context mContext;
+
+    public FirebaseQueryAdapter(Context context) {
+        Firebase mFirebaseRef = new Firebase(BuildConfig.FIREBASE_URL);
+        this.mRef = mFirebaseRef.limit(50);
+        this.mContext = context;
+
+        mModels = new ArrayList<>();
+        mKeys = new ArrayList<String>();
+        // Look for all child events. We will then map them to our own internal ArrayList, which backs ListView
+        mListener = this.mRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
+
+                TaskModel model = dataSnapshot.getValue(TaskModel.class);
+                String key = dataSnapshot.getKey();
+
+                // Insert into the correct location, based on previousChildName
+                if (previousChildName == null) {
+                    mModels.add(0, model);
+                    mKeys.add(0, key);
+                } else {
+                    int previousIndex = mKeys.indexOf(previousChildName);
+                    int nextIndex = previousIndex + 1;
+                    if (nextIndex == mModels.size()) {
+                        mModels.add(model);
+                        mKeys.add(key);
+                    } else {
+                        mModels.add(nextIndex, model);
+                        mKeys.add(nextIndex, key);
+                    }
+                }
+
+                notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                // One of the mModels changed. Replace it in our list and name mapping
+                String key = dataSnapshot.getKey();
+                TaskModel newModel = dataSnapshot.getValue(TaskModel.class);
+                int index = mKeys.indexOf(key);
+
+                mModels.set(index, newModel);
+
+                notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                // A model was removed from the list. Remove it from our list and the name mapping
+                String key = dataSnapshot.getKey();
+                int index = mKeys.indexOf(key);
+
+                mKeys.remove(index);
+                mModels.remove(index);
+
+                notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String previousChildName) {
+
+                // A model changed position in the list. Update our list accordingly
+                String key = dataSnapshot.getKey();
+                TaskModel newModel = dataSnapshot.getValue(TaskModel.class);
+                int index = mKeys.indexOf(key);
+                mModels.remove(index);
+                mKeys.remove(index);
+                if (previousChildName == null) {
+                    mModels.add(0, newModel);
+                    mKeys.add(0, key);
+                } else {
+                    int previousIndex = mKeys.indexOf(previousChildName);
+                    int nextIndex = previousIndex + 1;
+                    if (nextIndex == mModels.size()) {
+                        mModels.add(newModel);
+                        mKeys.add(key);
+                    } else {
+                        mModels.add(nextIndex, newModel);
+                        mKeys.add(nextIndex, key);
+                    }
+                }
+                notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                Log.e("FirebaseListAdapter", "Listen was cancelled, no more updates will occur");
+            }
+
+        });
+    }
+
+    public void cleanup() {
+        // We're being destroyed, let go of our mListener and forget about all of the mModels
+        mRef.removeEventListener(mListener);
+        mModels.clear();
+        mKeys.clear();
+    }
+
+    @Override
+    public int getCount() {
+        return mModels.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return mModels.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+
+    /*
+   Method called in the database change listener when a new change is detected.
+   Because live queries do not trigger a change event when non current revisions are saved
+   or pulled from a remote database.
+    */
+    public void updateQueryToShowConflictingRevisions(final Database.ChangeEvent event) {
+
+        ((Activity) FirebaseQueryAdapter.this.mContext).runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                notifyDataSetChanged();
+            }
+        });
+
+    }
+
+    /**
+     * Each time the data at the given Firebase location changes, this method will be called for each item that needs
+     * to be displayed. The arguments correspond to the mLayout and mModelClass given to the constructor of this class.
+     * <p>
+     * Your implementation should populate the view using the data contained in the model.
+     *
+     * @param v     The view to populate
+     * @param model The object containing the data used to populate the view
+     */
+//    protected abstract void populateView(View v, T model);
+}
