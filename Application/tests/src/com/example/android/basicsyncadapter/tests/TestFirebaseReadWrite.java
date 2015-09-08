82e7b2b0f277fda1495a1d972f696431de8b5781
cesine <cesine@yahoo.com>
2015-09-08 09:08:56 -0400

    aded test for firebase read write

---
+++ Application/tests/src/com/example/android/basicsyncadapter/tests/TestFirebaseReadWrite.java
@@ -0,0 +1,34 @@
+package com.example.android.basicsyncadapter.tests;
+
+import android.annotation.TargetApi;
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.example.android.basicsyncadapter.EntryListActivity;
+
+public class TestFirebaseReadWrite extends ActivityInstrumentationTestCase2<EntryListActivity> {
+
+    private EntryListActivity mTestActivity;
+
+    @TargetApi(8)
+    public TestFirebaseReadWrite() {
+        super(EntryListActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        mTestActivity = getActivity();
+        mTestActivity.getSupportFragmentManager().getFragments().get(0);
+    }
+
+    /**
+     * Test if the test fixture has been set up correctly.
+     */
+    public void testPreconditions() {
+        //Try to add a message to add context to your assertions. These messages will be shown if
+        //a tests fails and make it easy to understand why a test failed
+        assertNotNull("mTestActivity is null", mTestActivity);
+    }
+
+}
